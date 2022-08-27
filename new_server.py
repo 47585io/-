60f74d9c36ffb38c050a2f4b85af9a05ac3543d5
tom <442817485@qq.com>
2022-08-27 03:31:32 +0000

    modified:   new_server.py 	modified:   new_user.py 	modified:   test.py 	Untitled-1.py

--- new_server.py
+++ new_server.py
@@ -4,6 +4,7 @@ from concurrent import futures as fu
 import multiprocessing as mut
 from multiprocessing import Manager
 import atexit
+
 PRO_MAX = 1
 # 设置要为每个server init's process and sline count
 Mess_Buffer = 1024
@@ -34,11 +35,13 @@ class users:
         else:
             tmp[new_tup[0]]=new_tup[1]
             going_search_queue.put(tmp)
-    def getto(self,going_get_queue,index):
+    def getto(self,going_get_queue,index=None):
         '''get queue's dict or list index element'''
         tmp=going_get_queue.get()
         going_get_queue.put(tmp)
-        return tmp[index]
+        if index:
+            return tmp[index]
+        return tmp
     def add(self,tup):
         '''when a new user, call it'''
         self.search(self.users,tup)
@@ -48,6 +51,10 @@ class users:
         tmo=self.now_in.get()
         self.now_in.put(tmo)
         print(tmo)
+    def get_friend_list(self):
+        '''return user input's search after mess'''
+        tmp=self.getto(self.users)
+        return str(tmp.keys())       
     def Login(self,tup):
         '''when user login, call it'''
         name = tup[0].decode()
@@ -57,7 +64,12 @@ class users:
         self.add((name,tup[1]))
         friend=self.getto(self.friend_list,name)
         return name+str(friend)
-    def addfriend(self):
+    def value_to_key(self,tmp):
+        #print(self.getto(self.users)
+        for key, value in self.getto(self.users).items():
+            if tmp==value:
+                return key
+    def addfriend(self,tup):
         pass
         
 class message:
@@ -76,14 +88,25 @@ class message:
     #if user login, call login get username and friend_list, the end , send mess to src user
         if tmp[0].decode().startswith("LOGIN"):
             s = self.USERS.Login(tmp)
-            lis.extend((s.encode(), tmp[1]))
+            lis.extend((s, tmp[1]))
             return lis
-        #login return a decode str,OK,then let us to encode
-        
+       
+        #if user want to addfriend, return the users name
+        elif tmp[0].decode().startswith("AddFriend"):
+            lis.append(self.USERS.get_friend_list())
+            lis.append(tmp[1])
+            return lis
+                
     #default, the mess is name@str, read it , get the str and name, send str to name, and add mess from(my_name@str)
     #so send my_name@str to name's addr
         else:
-            pass
+            s_str, name = Spilt_Mess.Read_spilt(tmp[0])
+            addr=self.USERS.getto(self.USERS.users,name)
+            name=self.USERS.value_to_key(tmp[1])
+            s_str=Spilt_Mess.Send_spilt(s_str,name)
+            lis.append(s_str.decode())
+            lis.append(addr)
+            return lis
 
     def talk_to(self, *arg):
         '''the talk_to going to recv a bytes mess from a user, after process, it send a bytes mess to other user'''
@@ -96,8 +119,10 @@ class message:
 
     def Send(self, lis):
         '''send bytes, can redefine in sonclass'''
-        self.sock.sendto(lis[0], lis[1])
+        self.sock.sendto(lis[0].encode(), lis[1])
             #the lis[0] is from@str, lis[1] is send to addr
+        #login return a decode str,OK,then let us to encode
+
 
 
 class Group_Mess(message):
@@ -133,6 +158,45 @@ class Spilt_Mess:
             read_str = s_str[index+1::]
             name = s_str[0:index:]
             return (read_str, name)
+    
+    @staticmethod
+    def Friend_list_Send_Spilt(friend_list):
+        '''when send a friend_list, call it'''
+        s_str=""
+        for friend in friend_list:
+            s_str+="'"
+            s_str+=friend
+            s_str+="'"
+        return s_str.encode()
+    
+    @staticmethod
+    def Friend_list_Read_Spilt(s_str):
+        '''when read a friend_list,call it'''
+        s_str=s_str.decode()
+        friend_list=[]
+        start,end,index=(0,0,0)
+        while index<len(s_str):
+            if s_str[index]=="'":
+                start=index+1
+                index+=1
+                while True:
+                    if s_str[index] == "'":
+                        end=index
+                        friend_list.append(s_str[start:end:])
+                        break
+                    index+=1
+            index+=1
+        return friend_list
+    
+    @staticmethod
+    def Label_Add(s_str, num):
+        return (str(num)+':'+s_str).encode()
+
+    @staticmethod
+    def Label_Del(s_str):
+        s_str = s_str.decode()
+        index = s_str.find(':')
+        return (int(s_str[0:index:]), s_str[index+1::])
 
 
 def start(mess, arg):
@@ -155,7 +219,7 @@ def main(messes, arg):
 # but, you can append it's pointer many count in messes list, you can init many frequency
 # like below
 
-messes = [message(), Group_Mess()]
+messes = [message(),Group_Mess(),message(("127.0.0.1",1256))]
 messes.append(messes[0])
 # messes.append(users())
 
--- new_user.py
+++ new_user.py
@@ -1,13 +1,21 @@
+from email.mime import message
+from os import POSIX_FADV_RANDOM
 import socket
 import threading as th
 import atexit
 Mess_Buffer = 1024
+Max_Mess=10
+#recv buffer size and user mess_list size
 UDP_SOCK = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 UDP_SOCK.bind(("",0))
 TCP_SOCK=socket.socket()
 TCP_SOCK.bind(("", 0))
 #the user, olny two port with server
 USER_NAME=""
+Mess_Friend={}
+#记录与好友通信mess
+Talk_With=[]
+#who talk with now
 
 def whenexit():
     '''when exit, close all sock'''
@@ -15,38 +23,75 @@ def whenexit():
     TCP_SOCK.close()
 atexit.register(whenexit)
 
-
 class UDP_Mess:
     def __init__(self) -> None:
         self.MessCache = []
-        self.index = -1
-        self.NewMess = ""
-        self.yes = 0
-
+        self.index = -1   
+        self.yes=0    
+#Mess list: Max 10 mess,  index: now new mess index, yes:the mess yes or on new mess
     def get(self):
-        while 1:
-            if self.yes:
-                self.NewMess = self.MessCache[self.index]
-                del self.MessCache[self.index]
-                self.index -= 1
-                self.yes = 0
-                return self.NewMess
-
+        '''the func pop a element from MessCache head, and return'''
+        if self.index>=0:
+            mess = self.MessCache[0]
+            del self.MessCache[0]
+            self.index-=1
+            return mess
+    def getnew(self):
+        '''get best new mess'''
+        while self.yes==0:
+            pass
+        self.yes= 0
+        tmp=self.MessCache[self.index]
+        del self.MessCache[self.index]
+        self.index-=1
+        return tmp
+    
     def Send(self, sock,send_str, to_user=None):
+        '''when want send mess to server, call it'''
+        self.yes=0
+        #when send a mess, Read going to read a new mess, the yes=0, now index is no new
         send_str = Spilt_Mess.Send_spilt(send_str,to_user)
         sock.sendto(send_str, ("127.0.0.1", 1234))
 
     def Read(self,sock):
+        '''every once, recv a mess and add it to MessCache, if mess count >Max, del old mess'''
         while 1:
             tmp = sock.recvfrom(1024)
-            self.MessCache.append(Spilt_Mess.Read_spilt(tmp[0]))
-            self.yes = 1
+            self.MessCache.append(tmp[0])
             self.index += 1
-            if(self.index >= 10):
+            self.yes=1
+            if(self.index >= Max_Mess):
                 del self.MessCache[0]
                 self.index -= 1
             print(tmp[0])
+            
+UDP = UDP_Mess()
 
+
+class friends:
+    def __init__(self) -> None:
+        self.friend_list = []
+
+    def show(self):
+        return self.friend_list
+    
+    def format_list(self,list_):
+        new_list=[]
+        for name in list_:
+            if name not in self.friend_list and name != USER_NAME:
+                new_list.append(name)
+        return new_list
+    
+    def from_server_get_friend_list(self,name):
+        UDP.Send("AddFriend "+name)
+        s_str=UDP.getnew()
+        return Spilt_Mess.Friend_list_Read_Spilt(s_str)
+             
+    def addfriend(self):
+        list_=self.from_server_get_friend_list()
+        list_=self.format_list(list_)
+        self.friend_list.extend(list_)
+        
 class Spilt_Mess:
     @staticmethod
     def Send_spilt(s_str,name):
@@ -62,14 +107,43 @@ class Spilt_Mess:
         read_str = s_str[index+1::]
         name = s_str[0:index:]
         return (read_str, name)
-
+    
+    @staticmethod
+    def Friend_list_Send_Spilt(friend_list):
+        s_str=""
+        for friend in friend_list:
+            s_str+="'"
+            s_str+=friend
+            s_str+="'"
+        return s_str.encode()
+    
+    @staticmethod
+    def Friend_list_Read_Spilt(s_str):
+        s_str=s_str.decode()
+        friend_list=[]
+        start,end,index=(0,0,0)
+        while index<len(s_str):
+            if s_str[index]=="'":
+                start=index+1
+                index+=1
+                while True:
+                    if s_str[index] == "'":
+                        end=index
+                        friend_list.append(s_str[start:end:])
+                        break
+                    index+=1
+            index+=1
+        return friend_list
+        
 
 def main(mess,sock):
     r=th.Thread(target=mess.Read,args=(sock,))
     r.start()
+    #GNU_R=th.Thread(target=gra.show())
     while True:
         s=input()
         mess.Send(sock,s)
+        if s =="AddFriend":
+            print(Spilt_Mess.Friend_list_Read_Spilt(mess.get()))
  
-UDP=UDP_Mess()   
 main(UDP,UDP_SOCK)
